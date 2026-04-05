88fa0d7c534867572d60df0b16f1a2e976b57417
Joose2008 <jffernandezc@unimagdalena.edu.co>
2026-04-04 23:34:43 -0500

    Modificación de properties, y cambio en AbstractIT

--- src/test/java/unimag/plataformamedicos/domine/repositories/AbstractIntegrationTest.java
+++ src/test/java/unimag/plataformamedicos/domine/repositories/AbstractIntegrationTest.java
@@ -1,13 +1,13 @@
 package unimag.plataformamedicos.domine.repositories;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@DataJpaTest
 @Testcontainers
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
